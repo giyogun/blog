e532154125e554047a82811c936a65ea114d9dc5
Godwin Iyogun <giyogun@gmail.com>
2022-03-03 01:56:30 +0100

    fix CORS issue when making a post request

--- api/index.js
+++ api/index.js
@@ -32,6 +32,7 @@ app.post("/api/upload", upload.single("file"), (req,res) =>{
 
 app.use((req,res,next)=>{
   res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 })
 
